6d12f28b95737b48e1f4a3980483873739ab89a2
Spectral-Goose <taylorsingleton7567@gmail.com>
2021-04-14 23:18:22 -0400

    css update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,18 +4,43 @@ import { Item } from './Item';
 import { ItemsCollection } from '/imports/api/ItemsCollection';
 import { ItemForm } from './ItemForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+  ItemsCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteItem = ({ _id }) => ItemsCollection.remove(_id);
+
 export const App = () => {
-  const items = useTracker(() => ItemsCollection.find({}).fetch());
+  const items = useTracker(() => ItemsCollection.find({}, { sort: { createdAt: -1 } }).fetch());
   
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>Welcome to Meteor!</h1>
+          </div>
+        </div>
+      </header>
 
-      <ItemForm/>
+      <div className="main">
+        <ItemForm/>
 
-      <ul>
-        { items.map(item => <Item key={ item._id } item={ item }/>) }
-      </ul>
+        <ul className="items">
+          { items.map(item => (
+          <Item
+            key={ item._id }
+            item={ item }
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteItem}
+          />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- imports/ui/Item.jsx
+++ imports/ui/Item.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
-export const Item = ({ item }) => {
-    return <li>{item.text}</li>
+export const Item = ({ item, onCheckboxClick, onDeleteClick }) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={!!item.isChecked}
+        onClick={() => onCheckboxClick(item)}
+        readOnly
+      />
+      <span>{item.text}</span>
+      <button onClick={ () => onDeleteClick(item) }>&times;</button>
+    </li>
+  );
 };
